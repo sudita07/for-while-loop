c111de69bc237576d36da2fb9bbd884a3b2754ea
sudita07 <92347854+sudita07@users.noreply.github.com>
2021-10-11 20:22:37 -0700

    Add files via upload

---
+++ class14.py
@@ -0,0 +1,34 @@
+#Write a program to print first 10 natural number.
+
+# for i in range(1,11):
+#     print(i)
+# Write a program to print first 10 even numbers.
+# for i in range(0,11,2):
+#     print(i)
+# Write a program to print first 10 even numbers in reverse order
+
+# for i in range(10,-1,-2):
+#     print(i)
+# write a program to display the below pattern
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+# for i in range(5,0,-1):
+#     for j in range(0,i,1):
+#         print(i,end="")
+#     print()
+# write a program to print
+
+# 5
+# 4
+# 3
+# 2
+# 1 using while loop
+# i=5
+# while i>0:
+#     print(i)
+#     i=i-1
+
